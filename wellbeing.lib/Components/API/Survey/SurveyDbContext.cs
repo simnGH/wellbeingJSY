f6eb8c805eb1d704613630ac8ef2850af25fdd97
edknight91 <ed.knight1991@gmail.com>
2022-02-08 15:45:57 +0000

    add get questions for survey db query methods

--- wellbeing.lib/Components/API/Survey/SurveyDbContext.cs
+++ wellbeing.lib/Components/API/Survey/SurveyDbContext.cs
@@ -10,7 +10,11 @@ namespace wellbeing.Components.API.Survey
     {
         private const string COLS_SURVEY = "s.SurveyId, s.SurveyName, s.CreatedAt, s.UpdatedAt";
 
+        private const string COLS_QUESTION = "q.QuestionId, q.QuestionText, q.MetricId, q.CreatedAt, q.UpdatedAt";
+
         private static readonly string GetSurveyIdQuery = $"SELECT {COLS_SURVEY} FROM survey s WHERE s.SurveyId = @SurveyId;";
+
+        private static readonly string GetQuestionsForSurveyQuery = $"SELECT @QuestionText FROM question ORDER BY RAND() LIMIT 0, 3";
 
 
         public static ISurveyDbContext Current { get; set; }
@@ -26,5 +30,15 @@ namespace wellbeing.Components.API.Survey
             return result.AsEnumerable().FirstOrDefault();
         }
 
+        public async Task<DataRow> GetQuestionsForSurvey(string questionText)
+        {
+            DbParameter[] parameters =
+            {
+                new DbParameter("@QuestionText", DbType.String, questionText),
+            };
+
+            DataTable questions = await this.Execute<DataTable>(GetQuestionsForSurveyQuery, parameters);
+            return questions.AsEnumerable().FirstOrDefault();
+        }
     }
 }
