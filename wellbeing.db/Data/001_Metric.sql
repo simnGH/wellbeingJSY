23ada22cf7198635031303db0df396c477bbce8f
K4YL3Y <kayleyprice20@gmail.com>
2022-02-15 19:53:59 +0000

    data tables update

---
+++ wellbeing.db/Data/001_Metric.sql
@@ -0,0 +1,6 @@
+INSERT INTO metric (MetricId, MetricName)
+VALUES
+(1, "Mind"), 
+(2, "Body"),
+(3, "Wealth"),
+(4, "Work"),
---
+++ wellbeing.db/Data/001_Question.sql
@@ -0,0 +1,19 @@
+INSERT INTO question (QuestionId, QuestionText, MetricId)
+VALUES
+(1, "How did you feel today?", 1),
+(2, "How healthy have you been today?", 2),
+(3, "How have you coped financially today?", 3),
+(4, "How was work today?", 4),
+(5, "How is your quality of sleep?", 5),
+(6, "How has my appetite been recently?", 6),
+(7, "How relaxed do I feel currently?", 7),
+(8, "What are my energy levels like when I finish work?", 8),
+(9, "How organized have you felt at work recently?", 9),
+(10, "How are you coping with any debts you currently have?", 10)
+(11, "Have you had any problems with your work or daily life due to your physical health?", 11),
+(12, "Have you had any problems with your work or daily life due to any emotional problems, such as feeling depressed, sad or anxious?", 12),
+(13, "How often do you feel positive about your life?", 13),
+(14, "How healthy do you consider yourself?", 14),
+(15, "How active have you been recently?", 15),
+(16, "How often do you smoke?", 16),
+
